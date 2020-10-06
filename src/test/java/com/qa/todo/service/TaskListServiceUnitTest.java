1ac2df5a95dd368f4bf8a280c06024d3b8a595da
Sangiv Giovanni Karunakaran <skarunakaran@qa.com>
2020-10-06 16:13:58 +0100

    completed test

--- src/test/java/com/qa/todo/service/TaskListServiceUnitTest.java
+++ src/test/java/com/qa/todo/service/TaskListServiceUnitTest.java
@@ -53,35 +53,26 @@ class TaskListServiceUnitTest {
 
     @Test
     void createTest() {
-        // a when() to set up our mocked repo
-        when(this.repo.save(this.testClub)).thenReturn(this.testClubWithId);
 
-        // and a when() to set up our mocked modelMapper
-        when(this.modelMapper.map(this.testClubWithId, ClubDTO.class)).thenReturn(this.clubDTO);
+        when(this.repo.save(this.testTasklist)).thenReturn(this.testTasklistWithId);
 
-        // check that the clubDTO set up as our <expected> value
-        // is the same as the <actual> result when running the service.create()
-        // method
+        when(this.modelMapper.map(this.testTasklistWithId, TaskListDTO.class)).thenReturn(this.tasklistDTO);
 
-        ClubDTO expected = this.clubDTO;
-        ClubDTO actual = this.service.createClub(this.testClub);
+        TaskListDTO expected = this.tasklistDTO;
+        TaskListDTO actual = this.service.create(this.testTasklist);
         assertThat(expected).isEqualTo(actual);
 
-        // we check that our mocked repository was hit - if it was, that means our
-        // service works
-        verify(this.repo, times(1)).save(this.testClub);
+        verify(this.repo, times(1)).save(this.testTasklist);
     }
 
     @Test
     void readOneTest() {
-        // the repo spun up extends a Spring type of repo that uses Optionals
-        // thus, when running a method in the repo (e.g. findById() ) we have to
-        // return the object we want as an Optional (using Optional.of(our object) )
-        when(this.repo.findById(this.id)).thenReturn(Optional.of(this.testClubWithId));
 
-        when(this.modelMapper.map(this.testClubWithId, ClubDTO.class)).thenReturn(this.clubDTO);
+        when(this.repo.findById(this.id)).thenReturn(Optional.of(this.testTasklistWithId));
 
-        assertThat(this.clubDTO).isEqualTo(this.service.readOne(this.id));
+        when(this.modelMapper.map(this.testTasklistWithId, TaskListDTO.class)).thenReturn(this.tasklistDTO);
+
+        assertThat(this.tasklistDTO).isEqualTo(this.service.readOne(this.id));
 
         verify(this.repo, times(1)).findById(this.id);
     }
@@ -91,59 +82,46 @@ class TaskListServiceUnitTest {
         // findAll() returns a list, which is handy, since we already have clubs spun up.
         when(this.repo.findAll()).thenReturn(this.tasklists);
 
-        when(this.modelMapper.map(this.testClubWithId, ClubDTO.class)).thenReturn(this.clubDTO);
+        when(this.modelMapper.map(this.testTasklistWithId, TaskListDTO.class)).thenReturn(this.tasklistDTO);
 
-        assertThat(this.service.readAllClubs().isEmpty()).isFalse();
+        assertThat(this.service.readAll().isEmpty()).isFalse();
 
         verify(this.repo, times(1)).findAll();
     }
 
     @Test
     void updateTest() {
-        Club club = new Club("BVB", 250000000L);
-        club.setId(this.id);
+        TaskList tasklist = new TaskList("BVB");
+        tasklist.setId(this.id);
 
-        ClubDTO clubDTO = new ClubDTO(null, "BVB", 250000000L, null);
+        TaskListDTO tasklistDTO = new TaskListDTO(null, "BVB", null);
 
-        Club updatedClub= new Club(clubDTO.getName(), clubDTO.getValue());
-        updatedClub.setId(this.id);
+        TaskList updatedTasklist = new TaskList(tasklistDTO.getName());
+        updatedTasklist.setId(this.id);
 
-        ClubDTO updatedClubDTO = new ClubDTO(this.id, updatedClub.getName(),
-                updatedClub.getValue(), null);
+        TaskListDTO updatedTasklistDTO = new TaskListDTO(this.id, updatedTasklist.getName(), null);
 
-        // finById() grabs a specific club out of the repo
-        when(this.repo.findById(this.id)).thenReturn(Optional.of(club));
+        when(this.repo.findById(this.id)).thenReturn(Optional.of(tasklist));
 
-        // we then save() an updated club back to the repo
-        // we'd normally save() it once we've done stuff to it, but instead we're just
-        // feeding in an <expected>
-        // that we set up at the top of this method ^
-        when(this.repo.save(club)).thenReturn(updatedClub);
+        when(this.repo.save(tasklist)).thenReturn(updatedTasklist);
 
-        when(this.modelMapper.map(updatedClub, ClubDTO.class)).thenReturn(updatedClubDTO);
+        when(this.modelMapper.map(updatedTasklist, TaskListDTO.class)).thenReturn(updatedTasklistDTO);
 
-        assertThat(updatedClubDTO).isEqualTo(this.service.update(clubDTO, this.id));
+        assertThat(updatedTasklistDTO).isEqualTo(this.service.update(tasklistDTO, this.id));
 
-        // since we've ran two when().thenReturn() methods, we need to run a verify() on
-        // each:
         verify(this.repo, times(1)).findById(1L);
-        verify(this.repo, times(1)).save(updatedClub);
+        verify(this.repo, times(1)).save(updatedTasklist);
     }
 
     @Test
     void deleteTest() {
-        // running this.repo.existsById(id) twice, hence two returns (true &
-        // false)
-        // the <true> and <false> get plugged in once each to our two verify() methods
+
         when(this.repo.existsById(id)).thenReturn(true, false);
 
         assertThat(this.service.delete(id)).isTrue();
 
-
         verify(this.repo, times(1)).deleteById(id);
-        
-        // this plugs in the <true> from our when().thenReturn()
-        // this plugs in the <false> from our when().thenReturn()
+
         verify(this.repo, times(2)).existsById(id);
     }
 
