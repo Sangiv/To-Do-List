e6848cddf56825c3a8b4279557c77f82683c2eb2
Sangiv Giovanni Karunakaran <skarunakaran@qa.com>
2020-10-08 13:27:32 +0100

    completed selenium tests

--- src/test/java/com/qa/todo/selenium/UITesting.java
+++ src/test/java/com/qa/todo/selenium/UITesting.java
@@ -1,8 +1,9 @@
 package com.qa.todo.selenium;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
@@ -34,6 +35,7 @@ public class UITesting {
         assertThat("To-Do-List").isEqualTo(driver.getTitle());
     }
     
+    
     @Test
     public void testCreatePage() throws InterruptedException {
         driver.get("http://localhost:8901/index.html");
@@ -43,6 +45,7 @@ public class UITesting {
 
         assertThat("Create A Task List").isEqualTo(driver.getTitle());
     }
+    
     
     @Test
     public void testCreateTaskList() throws InterruptedException {
@@ -58,12 +61,26 @@ public class UITesting {
         driver.findElement(By.xpath("/html/body/div/a")).click();
         
 //        is table now displayed?
+        Thread.sleep(1500);
         assertThat(driver.findElement(By.xpath("/html/body/table")).isDisplayed());
     }
+    
     
     @Test
     public void testViewPage() throws InterruptedException {
         driver.get("http://localhost:8901/index.html");
+        
+//      navigate to create TaskList page
+        driver.findElement(By.xpath("/html/body/a")).click();
+        
+//      create a TaskList
+        WebElement createBar = driver.findElement(By.id("inputName"));
+        String createTerm = "End-Of-Season Tagets";
+        createBar.sendKeys(createTerm);
+        createBar.submit();
+        
+//      Navigate back to homepage
+        driver.findElement(By.xpath("/html/body/div/a")).click();
         
 //        navigate to view TaskList page
         driver.findElement(By.xpath("/html/body/table/tbody/tr/td[3]/a")).click();
@@ -71,26 +88,168 @@ public class UITesting {
         assertThat("Task List Details").isEqualTo(driver.getTitle());
     }
     
+    
     @Test
     public void testUpdateTaskList() throws InterruptedException {
-        driver.get("http://localhost:8901/record.html?id=1");
+        driver.get("http://localhost:8901/index.html");
+        
+//      navigate to create TaskList page
+        driver.findElement(By.xpath("/html/body/a")).click();
+        
+//      create a TaskList
+        WebElement createBar = driver.findElement(By.id("inputName"));
+        String createTerm = "End-Of-Season Tagets";
+        createBar.sendKeys(createTerm);
+        createBar.submit();
+        
+//      Navigate back to homepage
+        driver.findElement(By.xpath("/html/body/div/a")).click();
+        
+//        navigate to view TaskList page
+        driver.findElement(By.xpath("/html/body/table/tbody/tr/td[3]/a")).click();
         
 //        find update textbox
         WebElement updateBar = driver.findElement(By.id("inputName"));
         String updateTerm = "End-Of-Season Goals";
+        updateBar.clear();
         updateBar.sendKeys(updateTerm);
         updateBar.submit();
         
 //        navigate back to home
         driver.findElement(By.xpath("/html/body/div/a[1]")).click();
         
+        
+        List<WebElement> results;
+        results = driver.findElements(By.xpath("/html/body/table/tbody/tr/*"));
+        WebElement result = results.get(1);
+        
+        String testtext = result.getText();
+        
 //        is the name updated?
-        assertThat(updateTerm).isEqualTo(driver.findElement(By.xpath("/html/body/table/tbody/tr/td[2]")).getText());
+        Thread.sleep(1500);
+        assertThat(updateTerm).isEqualTo(testtext);
+           
     }
     
     
-    
+    @Test
+    public void testCreateTaskPage() throws InterruptedException {
+    	driver.get("http://localhost:8901/record.html?id=1");
+        
+//        click the create task button
+        driver.findElement(By.xpath("/html/body/div/a[2]")).click();
 
+        assertThat("Create A Task").isEqualTo(driver.getTitle());        
+    }
+    
+    
+    @Test
+    public void testCreateTask() throws InterruptedException {
+        driver.get("http://localhost:8901/index.html");
+        
+//      navigate to create TaskList page
+        driver.findElement(By.xpath("/html/body/a")).click();
+        
+//      create a TaskList
+        WebElement createBar = driver.findElement(By.id("inputName"));
+        String createTerm = "End-Of-Season Tagets";
+        createBar.sendKeys(createTerm);
+        createBar.submit();
+        
+//      Navigate back to homepage
+        driver.findElement(By.xpath("/html/body/div/a")).click();
+        
+//      navigate to view TaskList page
+        driver.findElement(By.xpath("/html/body/table/tbody/tr/td[3]/a")).click();
+        
+//      navigate to create task page
+        driver.findElement(By.xpath("/html/body/div/a[2]")).click();
+        
+//      create a Task
+        WebElement createBar2 = driver.findElement(By.id("inputName"));
+        WebElement idBar = driver.findElement(By.id("inputId"));
+        String createTerm2 = "End-Of-Season Tagets";
+        createBar2.sendKeys(createTerm2);
+        idBar.sendKeys("1");
+        createBar2.submit();
+      
+//      Navigate back to homepage
+        driver.findElement(By.xpath("/html/body/div/a")).click();
+//      Navigate to TaskList
+        driver.findElement(By.xpath("/html/body/table/tbody/tr[1]/td[3]/a")).click();
+      
+//      is table now displayed?
+        Thread.sleep(1500);
+        assertThat(driver.findElement(By.xpath("/html/body/div/table")).isDisplayed());       
+    }
+
+    
+    @Test
+    public void testDeleteTask() throws InterruptedException {
+        driver.get("http://localhost:8901/index.html");
+        
+//      navigate to create TaskList page
+        driver.findElement(By.xpath("/html/body/a")).click();
+        
+//      create a TaskList
+        WebElement createBar = driver.findElement(By.id("inputName"));
+        String createTerm = "End-Of-Season Tagets";
+        createBar.sendKeys(createTerm);
+        createBar.submit();
+        
+//      Navigate back to homepage
+        driver.findElement(By.xpath("/html/body/div/a")).click();
+        
+//      navigate to view TaskList page
+        driver.findElement(By.xpath("/html/body/table/tbody/tr/td[3]/a")).click();
+        
+//      navigate to create task page
+        driver.findElement(By.xpath("/html/body/div/a[2]")).click();
+        
+//      create a Task
+        WebElement createBar2 = driver.findElement(By.id("inputName"));
+        WebElement idBar = driver.findElement(By.id("inputId"));
+        String createTerm2 = "End-Of-Season Tagets";
+        createBar2.sendKeys(createTerm2);
+        idBar.sendKeys("1");
+        createBar2.submit();
+      
+//      Navigate back to homepage
+        driver.findElement(By.xpath("/html/body/div/a")).click();
+//      Navigate to TaskList
+        driver.findElement(By.xpath("/html/body/table/tbody/tr[1]/td[3]/a")).click();
+        
+//        click the delete task button
+        driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[4]/a")).click();
+        
+//        is the task still there?
+        Thread.sleep(3000);
+        assertFalse(driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]")).isDisplayed());       
+    }
+    
+    
+    @Test
+    public void testDeleteTaskList() throws InterruptedException {
+        driver.get("http://localhost:8901/createlist.html");
+        
+//      create a TaskList
+        WebElement createBar = driver.findElement(By.id("inputName"));
+        String createTerm = "End-Of-Season Tagets";
+        createBar.sendKeys(createTerm);
+        createBar.submit();
+      
+//      Navigate back to homepage
+        driver.findElement(By.xpath("/html/body/div/a")).click();
+        
+//        click the delete task button
+        driver.findElement(By.xpath("/html/body/table/tbody/tr/td[4]/a")).click();
+        
+//        is the task still there?
+        Thread.sleep(3000);
+        assertFalse(driver.findElement(By.xpath("/html/body/table")).isDisplayed());       
+    }
+    
+    
     @AfterClass
     public static void tearDown() {
         driver.close();
